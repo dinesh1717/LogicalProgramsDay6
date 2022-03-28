72eafd5a97eb0a01cbdcac6d1a1aa2d8382bad44
dinesh nangare <dineshnangare1717@gmail.com>
2022-03-28 19:34:27 +0530

    Day6 UC1

---
+++ FibonacciSeries.java
@@ -0,0 +1,21 @@
+package Day6;
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many times you want to print the Fibonacci_Series : ");
+        int times = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " " + b);
+        int c;
+        for (int i=1; i<=times; i++) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+    }
+
+}
